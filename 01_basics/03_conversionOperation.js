7884a764d8d87873d5e6e064cf9e020ee6aa4c02
Aman Kumar Tripathi <103057189+tripathiaman9354@users.noreply.github.com>
2025-01-27 16:15:23 +0000

    conversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,34 @@
+let score=undefined
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+
+let valueInNumber=Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" =>NaN(not a number)
+// undefined => NaN
+// null => 0
+// true => 1
+// false => 0
+
+ let isLoggedIn = "aman"
+
+ let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+ 
+
+ // 1 => true
+ // 0 => false
+ // "" => false
+ // "aman" => true
+
+
+ let someNumber = 45
+ let stringNumber = String(someNumber)
+ console.log(stringNumber)
+ console.log(typeof stringNumber);
+ 
