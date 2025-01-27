eeafb03805bfae80081558d1f4687de18aa75cda
Aman Kumar Tripathi <103057189+tripathiaman9354@users.noreply.github.com>
2025-01-27 16:45:48 +0000

    Operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -29,6 +29,45 @@ let valueInNumber=Number(score)
 
  let someNumber = 45
  let stringNumber = String(someNumber)
- console.log(stringNumber)
- console.log(typeof stringNumber);
+//  console.log(stringNumber)
+//  console.log(typeof stringNumber);
  
+/****************************Operation***************************/
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/3);
+// console.log(2%2);
+
+
+
+let str1="Hello"
+let str2=" Aman"
+// console.log(str1+str2  );
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(2 + 2 + "5");
+
+// console.log(+"");
+// console.log(+true);
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+// gameCounter++
+++gameCounter
+console.log(gameCounter);
+
+
+
+
